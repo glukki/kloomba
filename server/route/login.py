54ff7452c341056e8c378720480099baa88d4806
Vitalii Meshchaninov <glukki.spb.ru@gmail.com>
2012-06-12 01:59:37 +0400

    Login creates new gamer on first login Returns user profile, backpack, rules, first login flag

--- server/route/login.py
+++ server/route/login.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 import time
+import hashlib
 from google.appengine.api import users
-from main import ProtobufHandler, DEBUG
+from google.appengine.ext.db import Key
+from main import ProtobufHandler, DEBUG, SALT, TICK, FLOWERS_PER_TICK
 import kloombaDb
 from message.Login_pb2 import Login
 
@@ -16,33 +18,62 @@ class LoginHandler(ProtobufHandler):
         r = Login()
 
         user = users.get_current_user()
-        q = kloombaDb.Gamer.all()
-        q.filter('user =', user)
-        gamer = q.fetch(1)
+        gamer_key = Key.from_path(kloombaDb.Gamer.kind(), user.user_id())
+        gamer = kloombaDb.Gamer.all().ancestor(gamer_key).get()
         if not gamer:
             #make new account
-            gamer = kloombaDb.Gamer()
+            gamer = kloombaDb.Gamer(parent=gamer_key)
+            gamer.user_id = user.user_id()
+            gamer.public_id = hashlib.md5(user.user_id() + SALT).hexdigest()
             gamer.name = user.nickname()
-            gamer.level = 1
-            gamer.experience = 0
             gamer.put()
+            #make new backpack
+            backpack = []
+            #add flowers
+            bp = kloombaDb.Backpack(parent=gamer_key)
+            bp.owner = user.user_id()
+            bp.name = 'flower'
+            bp.amount = 100
+            bp.put()
+            backpack.append(bp)
+            #add flowerbeds
+            bp = kloombaDb.Backpack(parent=gamer_key)
+            bp.owner = user.user_id()
+            bp.name = 'flowerbed'
+            bp.amount = 100
+            bp.put()
+            backpack.append(bp)
             #set first_time
             r.first_time = True
         else:
-            gamer = gamer[0]
             gamer.put()
+            backpack = kloombaDb.Backpack.all().ancestor(gamer_key).run()
+
 
         #set user
+        r.user.timestamp = int(time.mktime(gamer.lastActiveTimestamp.timetuple()))
+        r.user.id = gamer.public_id
         r.user.name = gamer.name
         r.user.level = gamer.level
         r.user.experience = gamer.experience
+        #set backpack
+        r.user.backpack.timestamp = int(time.time())
+        for i in backpack:
+            bp = r.user.backpack.item.add()
+            bp.name = i.name
+            bp.amount = i.amount
+
 
         #TODO: replace with rules update timestamp
-        if int(self.request.get('ts', 0)) < int(time.time()):
+        if int(self.request.get('ts', 0)) < 1339436729:
             #set rules
+            r.rules.timestamp = 1339436729
             rule = r.rules.item.add()
-            rule.name = 'test'
-            rule.value = 'val'
+            rule.name = 'TICK'
+            rule.value = str(TICK)
+            rule = r.rules.item.add()
+            rule.name = 'FLOWERS_PER_TICK'
+            rule.value = str(FLOWERS_PER_TICK)
 
         if DEBUG:
             self.response.out.write(r)
