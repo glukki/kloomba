42c44de0d48d28aac3edbaa5cb9e7e4033ad503f
Vitalii Meshchaninov <glukki.spb.ru@gmail.com>
2012-06-12 02:08:35 +0400

    Flowerbed module finished
    FlowerbedExplore: no adjacent tiles searching
    FlowerbedAdd: no check for distance from new flowerbed to existent, no check for distance from new flowerbed to user position
    FlowerbedTransfer: no check for distance from flowerbed to user position

--- server/route/flowerbed.py
+++ server/route/flowerbed.py
@@ -1,11 +1,19 @@
 # coding=utf-8
-from main import ProtobufHandler, DEBUG
+import hashlib
+from math import floor
+import time
+from google.appengine.ext.db import Key, GeoPt
+from geo import geocell
+from google.appengine.api import users
+from main import ProtobufHandler, DEBUG, SALT, TICK, FLOWERS_PER_TICK
+import kloombaDb
 from message.FlowerbedAdd_pb2 import FlowerbedAdd
 from message.FlowerbedExplore_pb2 import FlowerbedExplore
 from message.FlowerbedTransfer_pb2 import FlowerbedTransfer
 
 __author__ = 'Vitaliy (GLuKKi) Meshchaninov glukki.spb.ru@gmail.com'
 
+#noinspection PyUnresolvedReferences
 class FlowerbedExploreHandler(ProtobufHandler):
     """
     ancestor: tile
@@ -15,12 +23,35 @@ class FlowerbedExploreHandler(ProtobufHandler):
     def get(self):
         r = FlowerbedExplore()
 
+        user = users.get_current_user()
+        gamer_key = Key.from_path(kloombaDb.Gamer.kind(), user.user_id())
+        gamer = kloombaDb.Gamer.all().ancestor(gamer_key).get()
+
+        if self.request.get('lat') and self.request.get('lon'):
+            r.timestamp = int(time.time())
+            lat = float(self.request.get('lat')) / 1000000
+            lon = float(self.request.get('lon')) / 1000000
+            gamer.point = GeoPt(lat, lon) #update user position
+            #TODO: take adjacent tiles
+            flowerbeds = kloombaDb.Flowerbed.all().ancestor(Key.from_path(kloombaDb.Flowerbed.kind(), geocell.compute(GeoPt(lat, lon)))).run()
+            for i in flowerbeds:
+                fb = r.flowerbed.add()
+                fb.timestamp = int(time.mktime(i.timestamp.timetuple()))
+                fb.id = str(i.key())
+                fb.latitude = int(i.point.lat * 1000000)
+                fb.longitude = int(i.point.lon * 1000000)
+                fb.owner = i.owner_public_id
+                fb.flowers = i.flowers
+
+        gamer.put()
+
         if DEBUG:
             self.response.out.write(r)
         else:
             self.response.out.write(r.SerializeToString())
 
 
+#noinspection PyUnresolvedReferences
 class FlowerbedAddHandler(ProtobufHandler):
     """
     ancestor: tile
@@ -28,7 +59,56 @@ class FlowerbedAddHandler(ProtobufHandler):
     response: State(Flowerbed, User)
     """
     def get(self):
+        user = users.get_current_user()
         r = FlowerbedAdd()
+        if self.request.get('lat') and self.request.get('lon'):
+            #TODO: check if flowerbed is far enough from others
+            #TODO: check if flowerbed is close enough to user
+            lat = float(self.request.get('lat')) / 1000000
+            lon = float(self.request.get('lon')) / 1000000
+            gamer_key = Key.from_path(kloombaDb.Gamer.kind(), user.user_id())
+            kloombaDb.Gamer.all().ancestor(gamer_key).get().put()
+            gamer_hash = hashlib.md5(user.user_id() + SALT).hexdigest()
+            r.timestamp = int(time.time())
+            #get backpack
+            bp_beds = kloombaDb.Backpack.all().ancestor(gamer_key).filter('name =', 'flowerbed').get()
+            if bp_beds.amount:
+                #lower backpack
+                bp_beds.amount -= 1
+                bp_beds.put()
+
+                #add flowerbed
+                point = GeoPt(lat, lon)
+                cell = geocell.compute(point)
+                flowerbed = kloombaDb.Flowerbed(parent=Key.from_path(kloombaDb.Flowerbed.kind(), cell))
+                flowerbed.point = point
+                flowerbed.tile = cell
+                flowerbed.owner = user.user_id()
+                flowerbed.owner_public_id = gamer_hash
+                flowerbed.put()
+                possession = kloombaDb.Possession(parent=gamer_key)
+                possession.flowerbed = flowerbed
+                possession.put()
+
+                backpack = kloombaDb.Backpack.all().ancestor(gamer_key)
+
+                #set timestamps
+                r.flowerbed.timestamp = int(time.time())
+                r.backpack.timestamp = int(time.time())
+
+                #set flowerbed
+                r.flowerbed.id = str(flowerbed.key())
+                r.flowerbed.latitude = int(self.request.get('lat'))
+                r.flowerbed.longitude = int(self.request.get('lon'))
+                r.flowerbed.owner = gamer_hash
+                r.flowerbed.flowers = 0
+
+                #set backpack
+                for i in backpack:
+                    bp = r.backpack.item.add()
+                    bp.name = i.name
+                    bp.amount = i.amount
+
 
         if DEBUG:
             self.response.out.write(r)
@@ -36,14 +116,103 @@ class FlowerbedAddHandler(ProtobufHandler):
             self.response.out.write(r.SerializeToString())
 
 
+#noinspection PyUnresolvedReferences
 class FlowerbedTransferHandler(ProtobufHandler):
     """
     ancestor: tile
-    request: /flowerbed/transfer?id=1&amount=1&direction=to
+    request: /flowerbed/transfer?id=1&amount=1
     response: State(Flowerbed, User)
     """
     def get(self):
         r = FlowerbedTransfer()
+
+        if not int(self.request.get('amount'), 0):
+            return
+
+        fb_id = self.request.get('id')
+        amount = int(self.request.get('amount'))
+
+
+        flowerbed = kloombaDb.Flowerbed.get(fb_id)
+        ts = time.time()
+        if flowerbed:
+            fb_flowers = flowerbed.flowers +\
+                         int(floor(
+                             (ts - time.mktime(flowerbed.timestamp.timetuple())) / TICK *\
+                             FLOWERS_PER_TICK
+                         ))
+            user = users.get_current_user()
+            gamer_key = Key.from_path(kloombaDb.Gamer.kind(), user.user_id())
+            gamer_hash = hashlib.md5(user.user_id() + SALT).hexdigest()
+            bp_flowers = kloombaDb.Backpack.all().ancestor(gamer_key).filter('name =', 'flower').get()
+            kloombaDb.Gamer.all().ancestor(gamer_key).get().put()
+
+            #TODO: check if flowerbed is close to user
+            if amount < 0: #from
+                if flowerbed.owner != user.user_id():
+                    return #not your flowerbed
+                if fb_flowers < amount * -1:
+                    return #not enough on flowerbed
+                #ok, now substract
+                flowerbed.flowers = fb_flowers + amount
+                flowerbed.put()
+                bp_flowers.amount -= amount
+                bp_flowers.put()
+            else: #to
+                if bp_flowers.amount < amount:
+                    return #not enough in backpack
+                if flowerbed.owner == user.user_id():
+                    #ok, it's mine
+                    flowerbed.flowers = fb_flowers + amount
+                    flowerbed.put()
+                    bp_flowers.amount -= amount
+                    bp_flowers.put()
+                else: #ok, attack
+                    if fb_flowers - amount >= 0:
+                        #still the same owner
+                        flowerbed.flowers = fb_flowers - amount
+                        flowerbed.put()
+                        bp_flowers.amount -= amount
+                        bp_flowers.put()
+                    else: #conquer
+                        #set lost
+                        lost = kloombaDb.Possession.all().ancestor(Key.from_path(kloombaDb.Gamer.kind(), flowerbed.owner)).filter('flowerbed =', flowerbed).get()
+                        lost.lost = True
+                        lost.put()
+                        #set flowerbed
+                        flowerbed.flowers = fb_flowers - amount
+                        flowerbed.owner = user.user_id()
+                        flowerbed.owner_public_id = gamer_hash
+                        flowerbed.put()
+                        #set backpack
+                        bp_flowers.amount -= amount
+                        bp_flowers.put()
+                        #set conquer
+                        possession = kloombaDb.Possession.all().ancestor(gamer_key).filter('flowerbed =', flowerbed).run()
+                        if not possession:
+                            possession = kloombaDb.Possession()
+                            possession.flowerbed = flowerbed
+                        possession.lost = False
+                        possession.put()
+
+            backpack = kloombaDb.Backpack.all().ancestor(gamer_key)
+
+            #set timestamps
+            r.flowerbed.timestamp = int(time.time())
+            r.backpack.timestamp = int(time.time())
+
+            #set flowerbed
+            r.flowerbed.id = str(flowerbed.key())
+            r.flowerbed.latitude = int(flowerbed.point.lat * 1000000)
+            r.flowerbed.longitude = int(flowerbed.point.lon * 1000000)
+            r.flowerbed.owner = flowerbed.owner_public_id
+            r.flowerbed.flowers = flowerbed.flowers
+
+            #set backpack
+            for i in backpack:
+                bp = r.backpack.item.add()
+                bp.name = i.name
+                bp.amount = i.amount
 
         if DEBUG:
             self.response.out.write(r)
