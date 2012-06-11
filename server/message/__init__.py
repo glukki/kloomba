368be28d8f4f16f3c8eaacb06148829a84a1837e
Vitalii Meshchaninov <glukki.spb.ru@gmail.com>
2012-06-12 01:26:47 +0400

    Update protobuf generated classes

--- server/message/__init__.py
+++ server/message/__init__.py
@@ -1,1 +1,7 @@
+# coding=utf-8
 __author__ = 'glukki'
+
+#from google.net.proto2.python.public import descriptor
+#from google.net.proto2.python.public import message
+#from google.net.proto2.python.public import reflection
+#from google.net.proto2.proto import descriptor_pb2
