97a12b37a2865d58bdbe0a9182a77d24c2c5f5fa
Vitalii Meshchaninov <glukki.spb.ru@gmail.com>
2012-06-12 01:48:15 +0400

    Updated DB structure
    Gamer: user is now string and stores user_id, public_id stores hash from user_id+salt, point stores latest gamer position
    Flowerbed: mistaken latitude renamed to point, geohash renamed to tile, owner stores string, added owner_public_id, plants renamed to flowers
    FlowerbedLog: owner stores string
    Possession: expireTimestamp renamed to timestamp
    Bookmark: owner stores string
    Backpack: item renamed to name and now stores string, timestamp added

--- server/kloombaDb.py
+++ server/kloombaDb.py
@@ -3,64 +3,68 @@ __author__ = 'Vitaliy (GLuKKi) Meshchaninov glukki.spb.ru@gmail.com'
 
 from google.appengine.ext import db
 
-#ancestor: user
+#ancestor: user_id
 class Gamer(db.Model):
-    user = db.UserProperty(auto_current_user_add=True)
+    user_id = db.StringProperty()
+    public_id = db.StringProperty() #md5 hash
     name = db.StringProperty()
     avatar = db.IntegerProperty()
-    level = db.IntegerProperty()
-    experience = db.IntegerProperty()
+    level = db.IntegerProperty(default=1)
+    experience = db.IntegerProperty(default=0)
+    point = db.GeoPtProperty()
     registrationTimestamp = db.DateTimeProperty(auto_now_add=True)
     lastActiveTimestamp = db.DateTimeProperty(auto_now=True)
 
 #get surrounding flowerbeds
 #ancestor: tile
 class Flowerbed(db.Model):
-    latitude = db.GeoPtProperty()
-    geohash = db.StringProperty()
-    level = db.IntegerProperty()
-    owner = db.UserProperty()
-    plants = db.IntegerProperty()
-    timestamp = db.DateTimeProperty()
+    point = db.GeoPtProperty()
+    tile = db.StringProperty()
+    level = db.IntegerProperty(default=1)
+    owner = db.StringProperty()
+    owner_public_id = db.StringProperty()
+    flowers = db.IntegerProperty(default=0)
+    timestamp = db.DateTimeProperty(auto_now=True)
 
 #ancestor: flowerbed
 class FlowerbedLog(db.Model):
     flowerbed = db.ReferenceProperty(reference_class=Flowerbed)
-    owner = db.UserProperty()
+    owner = db.StringProperty()
     attackPlants = db.IntegerProperty()
     defensePlants = db.IntegerProperty()
     timestamp = db.DateTimeProperty(auto_now_add=True)
 
 #store users current and lost flowerbeds
-#ancestor: user
+#ancestor: user_id
 #send diff time in seconds, how long ago it was lost
 class Possession(db.Model):
     flowerbed = db.ReferenceProperty(reference_class=Flowerbed)
-    lost = db.BooleanProperty()
-    expireTimestamp = db.DateTimeProperty()
+    lost = db.BooleanProperty(default=False)
+    timestamp = db.DateProperty(auto_now=True)
 
-#ancestor: user
+#ancestor: user_id
 class Bookmark(db.Model):
-    owner = db.UserProperty()
+    owner = db.StringProperty()
     flowerbed = db.ReferenceProperty(reference_class=Flowerbed)
-    timestamp = db.DateTimeProperty()
+    timestamp = db.DateTimeProperty(auto_now=True)
 
-#ancestor: "rule"
+#ancestor: "rules"
 #tick, plantPerTick, lostFlowerbedTimeout
 class Rule(db.Model):
     name = db.StringProperty()
     value = db.IntegerProperty()
     timestamp = db.DateTimeProperty(auto_now=True)
 
+#ancestor: "shop"
 class Item(db.Model):
-    id = db.StringProperty()
-    name = db.StringProperty()
+    name = db.StringProperty() #flower, flowerbed
     moneyPrice = db.IntegerProperty()
     flowerPrice = db.IntegerProperty()
 
-#ancestor: user
+#ancestor: user_id
 class Backpack(db.Model):
-    owner = db.UserProperty()
-    item = db.ReferenceProperty(reference_class=Item)
+    owner = db.StringProperty()
+    name = db.StringProperty() #Item.name
     amount = db.IntegerProperty()
+    timestamp = db.DateTimeProperty(auto_now=True)
 
