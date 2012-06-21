02bc86396532051c1e3b29834d69ba9fd9d4c258
Vitalii Meshchaninov <glukki.spb.ru@gmail.com>
2012-06-21 17:14:44 +0400

    Added manual indexes Added indexing block on models properties

--- server/kloombaDb.py
+++ server/kloombaDb.py
@@ -7,31 +7,31 @@ from google.appengine.ext import db
 class Gamer(db.Model):
     user_id = db.StringProperty()
     public_id = db.StringProperty() #md5 hash
-    name = db.StringProperty()
-    avatar = db.IntegerProperty()
-    level = db.IntegerProperty(default=1)
-    experience = db.IntegerProperty(default=0)
-    point = db.GeoPtProperty()
-    registrationTimestamp = db.DateTimeProperty(auto_now_add=True)
-    lastActiveTimestamp = db.DateTimeProperty(auto_now=True)
+    name = db.StringProperty(indexed=False)
+    avatar = db.IntegerProperty(indexed=False)
+    level = db.IntegerProperty(indexed=False, default=1)
+    experience = db.IntegerProperty(indexed=False, default=0)
+    point = db.GeoPtProperty(indexed=False)
+    registrationTimestamp = db.DateTimeProperty(indexed=False, auto_now_add=True)
+    lastActiveTimestamp = db.DateTimeProperty(indexed=False, auto_now=True)
 
 #get surrounding flowerbeds
 #ancestor: tile
 class Flowerbed(db.Model):
-    point = db.GeoPtProperty()
-    tile = db.StringProperty()
-    level = db.IntegerProperty(default=1)
-    owner = db.StringProperty()
-    owner_public_id = db.StringProperty()
-    flowers = db.IntegerProperty(default=0)
-    timestamp = db.DateTimeProperty(auto_now=True)
+    point = db.GeoPtProperty(indexed=False)
+    tile = db.StringProperty(indexed=False)
+    level = db.IntegerProperty(indexed=False, default=1)
+    owner = db.StringProperty(indexed=False)
+    owner_public_id = db.StringProperty(indexed=False)
+    flowers = db.IntegerProperty(indexed=False, default=0)
+    timestamp = db.DateTimeProperty(indexed=False, auto_now=True)
 
 #ancestor: flowerbed
 class FlowerbedLog(db.Model):
-    flowerbed = db.ReferenceProperty(reference_class=Flowerbed)
+    flowerbed = db.ReferenceProperty(indexed=False, reference_class=Flowerbed)
     owner = db.StringProperty()
-    attackPlants = db.IntegerProperty()
-    defensePlants = db.IntegerProperty()
+    attackPlants = db.IntegerProperty(indexed=False)
+    defensePlants = db.IntegerProperty(indexed=False)
     timestamp = db.DateTimeProperty(auto_now_add=True)
 
 #store users current and lost flowerbeds
@@ -40,31 +40,31 @@ class FlowerbedLog(db.Model):
 class Possession(db.Model):
     flowerbed = db.ReferenceProperty(reference_class=Flowerbed)
     lost = db.BooleanProperty(default=False)
-    timestamp = db.DateTimeProperty(auto_now=True)
+    timestamp = db.DateTimeProperty(indexed=False, auto_now=True)
 
 #ancestor: user_id
 class Bookmark(db.Model):
-    owner = db.StringProperty()
-    flowerbed = db.ReferenceProperty(reference_class=Flowerbed)
-    timestamp = db.DateTimeProperty(auto_now=True)
+    owner = db.StringProperty(indexed=False)
+    flowerbed = db.ReferenceProperty(indexed=False, reference_class=Flowerbed)
+    timestamp = db.DateTimeProperty(indexed=False, auto_now=True)
 
 #ancestor: "rules"
 #tick, plantPerTick, lostFlowerbedTimeout
 class Rule(db.Model):
-    name = db.StringProperty()
-    value = db.IntegerProperty()
-    timestamp = db.DateTimeProperty(auto_now=True)
+    name = db.StringProperty(indexed=False)
+    value = db.IntegerProperty(indexed=False)
+    timestamp = db.DateTimeProperty(indexed=False, auto_now=True)
 
 #ancestor: "shop"
 class Item(db.Model):
     name = db.StringProperty() #flower, flowerbed
-    moneyPrice = db.IntegerProperty()
-    flowerPrice = db.IntegerProperty()
+    moneyPrice = db.IntegerProperty(indexed=False)
+    flowerPrice = db.IntegerProperty(indexed=False)
 
 #ancestor: user_id
 class Backpack(db.Model):
-    owner = db.StringProperty()
-    name = db.StringProperty() #Item.name
-    amount = db.IntegerProperty()
-    timestamp = db.DateTimeProperty(auto_now=True)
+    owner = db.StringProperty(indexed=False)
+    name = db.StringProperty(indexed=False) #Item.name
+    amount = db.IntegerProperty(indexed=False)
+    timestamp = db.DateTimeProperty(indexed=False, auto_now=True)
 
